e05077864c14c7eaf645c73e5b35919017ac769b
julienBelinga <julienbelinga29@gmail.com>
2020-01-30 21:13:23 +0100

    cleanning the code

--- Matiere.java
+++ Matiere.java
@@ -1,85 +1,87 @@
+
 import java.util.Scanner;
 
 public class Matiere {
-    
+
     //constructeur
-    Matiere(){}
-    
+    Matiere() {
+    }
+
     private String nom;
     private Note note;
     private Note moyenne;
     private int coefficient, nbNote, i;
     private String professeur;
-    private double  somme = 0,m_moyenne = 0;
+    private double somme = 0, m_moyenne = 0;
     private int coefSomme = 0;
-    private final double tabNoteValeur[] = {0};
-    private final int tabNoteCoef[] = {0};
-    
+    private double tabNoteValeur[];
+    private int tabNoteCoef[];
+    private Scanner sc = new Scanner(System.in);
+
     //accesseurs
-    public String getNom(){
-        return nom;
-    }
-    public double getValeur(){
-        return m_moyenne;
-    }
-    public int getCoef(){
-        return coefficient;
-    }
-    public Note getMoyenne(){
+    public String getNom() {return nom;}
+
+    public double getValeur() {return m_moyenne;}
+
+    public int getCoef() {return coefficient;}
+
+    public Note getMoyenne() {
         moyenne.valeur = m_moyenne;
         moyenne.coefficient = coefficient;
-        
+
         return moyenne;
     }
-    public String getProf(){
-        return professeur;
-    }
-    
+
+    public String getProf() {return professeur;}
+
     //mutateurs
-    public String setNom(){
-        Scanner saisieNom = new Scanner(System.in);
-        nom = saisieNom.nextLine();
-        
+    public String setNom() {
+        System.out.print("matière: ");
+        nom = sc.nextLine();
+
         return nom;
     }
-    public String setProf(){
-        Scanner saisieNom = new Scanner(System.in);
-        professeur = saisieNom.nextLine();
-        
+
+    public String setProf() {
+        System.out.print("proffeseur: ");
+        professeur = sc.nextLine();
+
         return professeur;
     }
-    public int setCoef(){
-        Scanner saisieCoef = new Scanner(System.in);
-        coefficient = saisieCoef.nextInt();
-        
+
+    public int setCoef() {
+        System.out.print("coefficient: ");
+        coefficient = sc.nextInt();
+
         return coefficient;
     }
-  
+
     //method
-    public void calculMoyenne(){
-        System.out.println("Saisissez votre nombre de note:");
-        Scanner saisieNbNote = new Scanner(System.in);
-        nbNote = saisieNbNote.nextInt();
-        
-        for(i = 0; i < nbNote; i++){
-            Scanner saisieNoteValeur = new Scanner(System.in);
-            tabNoteValeur[i] = saisieNoteValeur.nextDouble();
-            Scanner saisieNoteCoef = new Scanner(System.in);
-            tabNoteCoef[i] = saisieNoteCoef.nextInt();
-            
-            somme += tabNoteValeur[i]*tabNoteCoef[i];
+    public void calculMoyenne() {
+        System.out.print("Saisissez votre nombre de note:");
+        nbNote = sc.nextInt();
+        System.out.print("\n");
+
+        for (i = 1; i < nbNote; i++) {
+            Scanner sc1 = new Scanner(System.in);
+            System.out.print("note: ");
+            tabNoteValeur[i] = sc1.nextDouble();
+            System.out.print("coef: ");
+            tabNoteCoef[i] = sc1.nextInt();
+
+            somme += (tabNoteValeur[i]) * (tabNoteCoef[i]);
             coefSomme += tabNoteValeur[i];
         }
-        
+
         m_moyenne = somme / coefSomme;
     }
-    
-    public void afficher(){
-        System.out.println("Matière : "+getNom()+"\n Professeur: "+getProf()+"\n");
-        System.out.println("Moyenne: "+moyenne+"\n");
-        
-        for (i=0; i < nbNote ; i++){
-            System.out.println("note: "+tabNoteValeur[i]+"/ coef: "+tabNoteCoef[i]);
+
+    public void afficher() {
+        System.out.println("Matière : " + getNom() + "\nProfesseur: " + getProf());
+        System.out.println("Moyenne: " + moyenne);
+
+        for (i = 0; i < nbNote; i++) {
+            System.out.println("note: " + tabNoteValeur[i] + "/ncoef: " + tabNoteCoef[i]);
         }
-    }    
+    }
 }
